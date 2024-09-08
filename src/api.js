f7ebeaa43c76f8ad0bf11931be675610085146c0
SATYASWARUP DAS <153348365+satyaswarup140@users.noreply.github.com>
2024-09-09 02:44:16 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'e57695880emsh107a4e5b8c1713cp1889a7jsn286143dd1111',
+		'x-rapidapi-key': '', //use your own code
 		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "91889f436acdd1282a0ba1067608885e";
+export const WEATHER_API_KEY = ""; //use your own code
